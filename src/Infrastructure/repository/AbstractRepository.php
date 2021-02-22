fd3ce6942348d5d0547205754e53192aaf820724
Arthur Stuepp <arthur.stuepp@gmail.com>
2021-02-22 20:36:24 -0300

    cria função getbyfield-

--- src/Infrastructure/repository/AbstractRepository.php
+++ src/Infrastructure/repository/AbstractRepository.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 namespace App\Infrastructure\repository;
 
 use PDO;
+
 abstract class AbstractRepository
 {
-    
+
     protected PDO $pdo;
 
     public function __construct()
@@ -19,5 +20,18 @@ abstract class AbstractRepository
         );
         $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         $this->pdo->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);
+    }
+
+
+    protected function getByField(string $table, string $field,  $value)
+    {
+        $stmt = $this->pdo->prepare('SELECT * FROM '.$table.' where '.$field.' = :value');
+
+
+        $stmt->bindValue(':value', $value, PDO::PARAM_INT);
+
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
--- src/Infrastructure/repository/GenreRepository.php
+++ src/Infrastructure/repository/GenreRepository.php
@@ -14,7 +14,7 @@ class GenreRepository extends AbstractRepository
     public function save(Genre $genre): int
     {
 
-
+        var_dump($this->getByField('music','id',1));die;
         $stmt = $this->pdo->prepare('INSERT INTO genre(name) VALUES (:name)');
         $stmt->bindValue(':name', $genre->name);
 
@@ -23,5 +23,5 @@ class GenreRepository extends AbstractRepository
         return (int)$this->pdo->lastInsertId();
     }
 
-    
+
 }
